c3b5326ee2ad6d1820da4341322cae4214bb61e4
Abel Kelbessa <akelbessa@haverford.edu>
2022-07-07 09:44:57 -0700

    day 3 - search feature partially works

---
+++ Capstone/View Controllers/SurveyViewController.m
@@ -0,0 +1,31 @@
+//
+//  SurveyViewController.m
+//  Capstone
+//
+//  Created by Abel Kelbessa on 7/6/22.
+//
+
+#import "SurveyViewController.h"
+
+@interface SurveyViewController ()
+
+@end
+
+@implementation SurveyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
