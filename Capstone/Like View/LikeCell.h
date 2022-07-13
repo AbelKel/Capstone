8727415e1f844cdaa43770f473deab82a21f9825
Abel Kelbessa <akelbessa@haverford.edu>
2022-07-12 22:50:54 -0700

    attempting to load likes on Likes page

---
+++ Capstone/Like View/LikeCell.h
@@ -0,0 +1,16 @@
+//
+//  LikeCell.h
+//  Capstone
+//
+//  Created by Abel Kelbessa on 7/12/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LikeCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
