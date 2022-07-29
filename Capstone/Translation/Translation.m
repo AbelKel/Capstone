6ea0dbd5eff2682d7c4815464f39918b722db1ee
Abel Kelbessa <abel.urgessa@gmail.com>
2022-07-28 17:38:08 -0700

    working on translation

---
+++ Capstone/Translation/Translation.m
@@ -0,0 +1,43 @@
+//
+//  Translation.m
+//  Capstone
+//
+//  Created by Abel Kelbessa on 7/28/22.
+//
+
+#import "Translation.h"
+@import MLKit;
+
+@implementation Translation
+
++ (void)translateText:(NSString *)text {
+    [Translation translate:text translatedText:^(NSString *text, NSError *error) {
+        [Translation translatedText:text];
+    }];
+}
+
++ (void)translate:(NSString *)text translatedText:(void(^)(NSString *text, NSError *error))completion {
+    MLKTranslatorOptions *options =
+            [[MLKTranslatorOptions alloc] initWithSourceLanguage:MLKTranslateLanguageEnglish
+                                                  targetLanguage:MLKTranslateLanguageGerman];
+    MLKTranslator *englishGermanTranslator =
+            [MLKTranslator translatorWithOptions:options];
+    MLKModelDownloadConditions *conditions =
+        [[MLKModelDownloadConditions alloc] initWithAllowsCellularAccess:NO
+                                             allowsBackgroundDownloading:YES];
+    [englishGermanTranslator downloadModelIfNeededWithConditions:conditions
+                                                      completion:^(NSError *_Nullable error) {
+      if (error != nil) {
+        return;
+      } else {
+          [englishGermanTranslator translateText:text completion:^(NSString *_Nullable translatedText, NSError *_Nullable error) {
+            if (error != nil || translatedText == nil) {
+              return;
+            } else {
+                completion(translatedText, nil);
+            }
+          }];
+      }
+    }];
+}
+@end
