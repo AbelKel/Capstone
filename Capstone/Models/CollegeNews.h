12c296eb8a471565219a0d20fe28365ac47cc8a1
Abel Kelbessa <abel.urgessa@gmail.com>
2022-07-29 16:38:45 -0700

    finishing up on the news section

--- Capstone/Models/CollegeNews.h
+++ Capstone/Models/CollegeNews.h
@@ -12,7 +12,12 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (strong, nonatomic) NSString *title;
 @property (strong, nonatomic) NSString *newsDescription;
+@property (strong, nonatomic) NSString *content;
+@property (strong, nonatomic) NSString *author;
+@property (strong, nonatomic) NSString *url;
 @property (strong, nonatomic) NSString *imageUrl;
+@property (strong, nonatomic) NSString *articleAuthorName;
+@property (strong, nonatomic) NSString *publicationDate;
 - (instancetype)initWithDictionaryNews:(NSDictionary *)dictionary;
 + (NSMutableArray *)collegesWithArrayNews:(NSArray *)dictionaries;
 
---
+++ Capstone/News/NewsDetailsViewController.h
@@ -0,0 +1,18 @@
+//
+//  NewsDetailsViewController.h
+//  Capstone
+//
+//  Created by Abel Kelbessa on 7/29/22.
+//
+#import "CollegeNews.h"
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NewsDetailsViewController : UIViewController
+
+@property (strong, nonatomic) CollegeNews *collegeNews;
+
+@end
+
+NS_ASSUME_NONNULL_END
