8692cebefc6da17b46412ad0ee16f4eccad7ce13
Abel Kelbessa <abel.urgessa@gmail.com>
2022-07-29 13:24:47 -0700

    fixed bug?

---
+++ Capstone/Translate/Translate.h
@@ -0,0 +1,16 @@
+//
+//  Translate.h
+//  Capstone
+//
+//  Created by Abel Kelbessa on 7/29/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Translate : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Capstone/Translate/Translate.m
@@ -0,0 +1,12 @@
+//
+//  Translate.m
+//  Capstone
+//
+//  Created by Abel Kelbessa on 7/29/22.
+//
+
+#import "Translate.h"
+
+@implementation Translate
+
+@end
