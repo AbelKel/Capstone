bb877c288155bb420aeba251f4d7c2e0a9c8537f
Abel Kelbessa <abel.urgessa@gmail.com>
2022-07-31 15:04:18 -0700

    translation fix

---
+++ Capstone/Translation/Translate.h
@@ -0,0 +1,18 @@
+//
+//  Translate.h
+//  Capstone
+//
+//  Created by Abel Kelbessa on 7/31/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Translate : NSObject
+
++ (void)translateText:(NSString *)inputText;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Capstone/Translation/Translate.m
@@ -0,0 +1,44 @@
+//
+//  Translate.m
+//  Capstone
+//
+//  Created by Abel Kelbessa on 7/31/22.
+//
+
+#import "Translate.h"
+@import MLKit;
+
+@implementation Translate
+
++ (void)translateText:(NSString *)inputText {
+    __block NSString *translatedText;
+    [Translate translate:inputText translatedText:^(NSString *text, NSError *error) {
+        translatedText = text;
+    }];
+}
+
++ (void)translate:(NSString *)text translatedText:(void(^)(NSString *text, NSError *error))completion {
+    MLKTranslatorOptions *options =
+            [[MLKTranslatorOptions alloc] initWithSourceLanguage:MLKTranslateLanguageEnglish
+                                                  targetLanguage:MLKTranslateLanguageGerman];
+    MLKTranslator *englishGermanTranslator =
+            [MLKTranslator translatorWithOptions:options];
+    MLKModelDownloadConditions *conditions =
+        [[MLKModelDownloadConditions alloc] initWithAllowsCellularAccess:NO
+                                             allowsBackgroundDownloading:YES];
+    [englishGermanTranslator downloadModelIfNeededWithConditions:conditions
+                                                      completion:^(NSError *_Nullable error) {
+      if (error != nil) {
+        return;
+      } else {
+          [englishGermanTranslator translateText:text completion:^(NSString *_Nullable translatedText, NSError *_Nullable error) {
+            if (error != nil || translatedText == nil) {
+              return;
+            } else {
+                completion(translatedText, nil);
+            }
+          }];
+      }
+    }];
+}
+@end
