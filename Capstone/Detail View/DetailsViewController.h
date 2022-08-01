177000ec824fbd2b07bbdf2b3e3e27a3ec13bfd6
Abel Kelbessa <abel.urgessa@gmail.com>
2022-08-01 14:18:25 -0700

    directions to colleges works well :)

--- Capstone/Detail View/DetailsViewController.h
+++ Capstone/Detail View/DetailsViewController.h
@@ -7,7 +7,9 @@
 #import "UIKit/UIKit.h"
 #import "College.h"
 NS_ASSUME_NONNULL_BEGIN
-@interface DetailsViewController : UIViewController
+@interface DetailsViewController : UIViewController<CLLocationManagerDelegate, MKMapViewDelegate> {
+    CLLocationManager *locationManager;
+}
 @property (strong, nonatomic) College *college;
 @end
 NS_ASSUME_NONNULL_END
