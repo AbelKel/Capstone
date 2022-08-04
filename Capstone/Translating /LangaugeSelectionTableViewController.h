b2efb2c8aab4cb77b9090064b4f5e40c07a16fad
Abel Kelbessa <abel.urgessa@gmail.com>
2022-08-04 10:23:31 -0700

    translation fix. there were missing files in the last commit

---
+++ Capstone/Translating /LangaugeSelectionTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  LangaugeSelectionTableViewController.h
+//  Capstone
+//
+//  Created by Abel Kelbessa on 8/4/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LanguageTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
